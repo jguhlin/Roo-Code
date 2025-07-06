c4022a1184e1611614827c02c990ed823de5f1c4
Joseph Guhlin <joseph.guhlin@gmail.com>
2025-07-06 14:24:53 +1200

    fix types and props for reference index

--- src/shared/ExtensionMessage.ts
+++ src/shared/ExtensionMessage.ts
@@ -229,6 +229,9 @@ export type ExtensionState = Pick<
 	| "customCondensingPrompt"
 	| "codebaseIndexConfig"
 	| "codebaseIndexModels"
+	| "referenceIndexConfig"
+	| "referenceIndexModels"
+	| "llmConversationStoragePath"
 	| "profileThresholds"
 > & {
 	version: string
